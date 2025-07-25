df3099854e7ba148f7dc3128aac0870925d8573a
surajnikhade <surajnikhade18@gmail.com>
2025-07-25 13:15:57 +0530

    NODEJS-01: added order of route code

--- src/app.js
+++ src/app.js
@@ -1,18 +1,27 @@
 const express = require("express");
 
 const app = express();
+// order of route matters.
+app.use("/test", (req, res) => {
+  res.send("test from server");
+});
 
-app.use("/test",(req, res)=>{
-res.send("Hello from server");
-})
+app.use("/test/2", (req, res) => {
+  res.send("test/2 from server");
+});
 
-app.use('/hello',(req, res)=>{
-    res.send('response for hello');
-})
+app.use("/", (req, res) => {
+  res.send("/ called ");
+}); 
 
-app.use('/red' , (req, res)=>{
-    res.send('red is the response');
-})
-app.listen(3000 , ()=>{
-    console.log("run port 3000");
+app.use("/hello", (req, res) => {
+  res.send("response for hello");
+});
+
+app.use("/red", (req, res) => {
+  res.send("red is the response");
+});
+
+app.listen(3000, () => {
+  console.log("run port 3000");
 });
